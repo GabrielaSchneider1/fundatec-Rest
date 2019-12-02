c86459fa57c2d990d70a048c94b02c00013e3276
tecnico <gabisk@gmail.com>
2019-12-02 20:07:22 -0300

    Inclusao de Carros

--- src/main/java/br/com/fundatec/carro/repository/CarroRepository.java
+++ src/main/java/br/com/fundatec/carro/repository/CarroRepository.java
@@ -10,31 +10,46 @@ import java.util.List;
 @Repository
 public class CarroRepository {
 
+    private static List<Carro> listaCarros = null;
+
     public List<Carro> listarCarros (String nome) {
 
         List<Carro> listaResultado = new ArrayList<>();
 
-        for (Carro carro : listaCarros){
+        for (Carro carro : getListaCarros()){
             if(carro.getNome().toLowerCase().contains(nome.toLowerCase())){
                 listaResultado.add(carro);
             }
         }
 
-
-
         return listaResultado;
     }
 
     public Carro consultar(Long id){
-        for (Carro carro : listaCarros){
+        for (Carro carro : getListaCarros()){
             if (carro.getId().equals(id)){
                 return carro;
             }
         } return null;
     }
 
-    private static List<Carro> listaCarros =
-         Arrays.asList(new Carro(1L, "Celta","MAX1000"), new Carro ( 2L, "Mustang", "IDD 2555"));
+
+    private List<Carro> getListaCarros(){
+        if(listaCarros == null){
+            listaCarros = new ArrayList<>();
+            listaCarros.add(new Carro(1L, "Celta","MAX1000"));
+            listaCarros.add(new Carro ( 2L, "Mustang", "IDD 2555"));
+        }
+        return listaCarros;
     }
+
+
+
+    public Carro incluir(Carro carro) {
+        carro.setId(new Long(getListaCarros().size()+ 1));
+        listaCarros.add(carro);
+        return carro;
+    }
+}
 
 
